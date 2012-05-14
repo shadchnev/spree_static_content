f1cc08e9933a7ef5b1ce843008257273bba56967
Evgeny Shadchnev <evgeny.shadchnev@gmail.com>
2012-05-14 10:43:17 +0100

    updating deface to work with the latest edge

--- app/overrides/pages_in_sidebar.rb
+++ app/overrides/pages_in_sidebar.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/layouts/spree_application",
+Deface::Override.new(:virtual_path => "spree/shared/_sidebar",
                      :name => "pages_in_sidebar",
-                     :insert_bottom => "#sidebar",
+                     :insert_top => "#sidebar",
                      :partial => "spree/static_content/static_content_sidebar")
